7d8a5c763404ad97f34c8e4822dad9d48ef6d6db
Tom Hendrikx <tom@fourdigits.nl>
2022-11-21 17:07:08 +0100

    Fix black violations

--- wagtail_helpdesk/wagtail_hooks.py
+++ wagtail_helpdesk/wagtail_hooks.py
@@ -6,8 +6,11 @@ from wagtail.core import hooks
 @hooks.register("register_admin_urls")
 def register_admin_urls():
     urls = [
-        path('jsi18n/', JavaScriptCatalog.as_view(packages=['wagtail_helpdesk']), name='javascript_catalog'),
-
+        path(
+            "jsi18n/",
+            JavaScriptCatalog.as_view(packages=["wagtail_helpdesk"]),
+            name="javascript_catalog",
+        ),
         # Add your other URLs here, and they will appear under `/admin/wagtail_helpdesk/`
         # Note: you do not need to check for authentication in views added here, Wagtail does this for you!
     ]

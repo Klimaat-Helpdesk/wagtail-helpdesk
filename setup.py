20861dc7064fae02c5d1738455edca22bfd40ecf
Tom Hendrikx <tom@fourdigits.nl>
2022-11-22 17:38:13 +0100

    Allow uprading to Wagtail 2.16

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     ],
     install_requires=[
         "Django>=3.1,<4.0",
-        "Wagtail>=2.0,<2.16",
+        "Wagtail>=2.0,<2.17",
         "django-allauth==0.44.0",
         "django-crispy-forms==1.11.0",
         "python-gitlab==2.5.0",

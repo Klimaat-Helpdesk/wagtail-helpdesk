74209fde078a35d2729f12c6a7798677199c4b87
Casper Timmers <casper@fourdigits.nl>
2022-12-22 14:28:23 +0100

    Replace Dutch text by English text

--- wagtail_helpdesk/cms/views.py
+++ wagtail_helpdesk/cms/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
 from django.views.decorators.clickjacking import xframe_options_exempt
 
 from wagtail_helpdesk.cms.models import AnswerIndexPage
@@ -11,7 +12,7 @@ def iframe_search_widget(request):
         request,
         "wagtail_helpdesk/cms/iframe_search_widget.html",
         {
-            "title": request.GET.get("title", "Zoeken"),
+            "title": request.GET.get("title", _("Search")),
             "answers_page": f"{settings.BASE_URL}{AnswerIndexPage.objects.first().url}",
             "base_url": settings.BASE_URL,
         },

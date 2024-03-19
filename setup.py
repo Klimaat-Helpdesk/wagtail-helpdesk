96594a5670c69e9b693a32e147a0bb3955f56d10
Casper Timmers <casper@fourdigits.nl>
2024-03-19 09:52:58 +0100

    Add support for Wagtail 4.2

--- setup.py
+++ setup.py
@@ -40,10 +40,11 @@ setup(
         "Framework :: Wagtail :: 3",
         "Framework :: Wagtail :: 4",
         "Framework :: Wagtail :: 4.1",
+        "Framework :: Wagtail :: 4.2",
     ],
     install_requires=[
         "Django>=3.2.18,<4.3",
-        "Wagtail>=3.0,<4.2",
+        "Wagtail>=3.0,<4.3",
         "django-allauth>=0.44,<1.0",
         "django-crispy-forms>=1.0,<2.0",
         "argon2-cffi>=21.0,<22.0",
